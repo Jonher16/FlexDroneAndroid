514369cfc566b86f4b77b28b205d82d4c8e6633b
Jonher16 <1999.jon@gmail.com>
2022-05-19 12:23:01 +0200

    First commit

---
+++ android-uxsdk-beta-sample/src/main/java/com/dji/ux/beta/sample/SocketHandler.kt
@@ -0,0 +1,41 @@
+package com.dji.ux.beta.sample
+
+import com.dji.ux.beta.sample.util.Universal
+import io.socket.client.IO
+import io.socket.client.Socket
+import java.net.URISyntaxException
+
+object SocketHandler {
+
+    lateinit var mSocket: Socket
+
+    @Synchronized
+    fun setSocket() {
+        try {
+// "http://10.0.2.2:3000" is the network your Android emulator must use to join the localhost network on your computer
+// "http://localhost:3000/" will not work
+// If you want to use your physical phone you could use the your ip address plus :3000
+// This will allow your Android Emulator and physical device at your home to connect to the server
+            var ip = Universal.getIp()
+            var address = "http://"+ip+":5001"
+            mSocket = IO.socket(address)
+        } catch (e: URISyntaxException) {
+            System.out.println(e)
+        }
+    }
+
+    @Synchronized
+    fun getSocket(): Socket {
+        return mSocket
+    }
+
+    @Synchronized
+    fun establishConnection() {
+        mSocket.connect()
+    }
+
+    @Synchronized
+    fun closeConnection() {
+        mSocket.disconnect()
+    }
+}
---
+++ android-uxsdk-beta-sample/src/main/java/com/dji/ux/beta/sample/util/Universal.java
@@ -0,0 +1,15 @@
+package com.dji.ux.beta.sample.util;
+
+public class Universal {
+    public static String getIp() {
+        return ip;
+    }
+
+    public static void setIp(String ip) {
+        Universal.ip = ip;
+    }
+
+    private static String ip;
+
+
+}
